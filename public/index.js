e55d507ff45db85a0f993415919c4072f15ea745
Kesha Hall <k_renee10@yahoo.com>
2020-10-26 22:08:37 -0400

    cut and paste code

--- public/index.js
+++ public/index.js
@@ -18,7 +18,7 @@ function populateTotal() {
   // reduce transaction amounts to a single total value
   let total = transactions.reduce((total, t) => {
     return total + parseInt(t.value);
-  }, 0);
+    }, 0);
 
   let totalEl = document.querySelector("#total");
   totalEl.textContent = total;
